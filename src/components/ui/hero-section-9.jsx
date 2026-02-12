f14c5936f7dda5e6baf49386a8c0e9f5e1293879
Islom <islom@Isloms-MacBook-Air.local>
2026-02-12 15:22:44 +0500

    save pixelon.logo

--- src/components/ui/hero-section-9.jsx
+++ src/components/ui/hero-section-9.jsx
@@ -3,7 +3,6 @@
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { motion } from 'framer-motion'
-import Link from 'next/link'
 import Counter from './Counter'
 
 const container = {
@@ -75,17 +74,35 @@ export default function HeroSection({
 						variants={item}
 						className='flex gap-4 justify-center lg:justify-start'
 					>
-						{actions.map((a, i) => (
+						<a href='#about'>
 							<Button
-								key={i}
-								className={`bg-${a.background}  hover:bg-${a.background}/90`}
+								className={`bg-cyan-600 text-white  hover:bg-cyan-600/90`}
 								size='lg'
-								asChild
-								variant={a.variant}
 							>
-								<Link href={a.href}>{a.text}</Link>
+								Biz haqimizda{' '}
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									width='18'
+									height='18'
+									viewBox='0 0 24 24'
+									fill='currentColor'
+									class='icon icon-tabler icons-tabler-filled icon-tabler-caret-right'
+								>
+									<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+									<path d='M9 6c0 -.852 .986 -1.297 1.623 -.783l.084 .076l6 6a1 1 0 0 1 .083 1.32l-.083 .094l-6 6l-.094 .083l-.077 .054l-.096 .054l-.036 .017l-.067 .027l-.108 .032l-.053 .01l-.06 .01l-.057 .004l-.059 .002l-.059 -.002l-.058 -.005l-.06 -.009l-.052 -.01l-.108 -.032l-.067 -.027l-.132 -.07l-.09 -.065l-.081 -.073l-.083 -.094l-.054 -.077l-.054 -.096l-.017 -.036l-.027 -.067l-.032 -.108l-.01 -.053l-.01 -.06l-.004 -.057l-.002 -12.059z' />
+								</svg>
 							</Button>
-						))}
+						</a>
+
+						<a href='#xizmatlar'>
+							<Button
+								className={'border-cyan-500 hover:bg-cyan-400/10'}
+								size='lg'
+								variant={'outline'}
+							>
+								Xizmatlar
+							</Button>
+						</a>
 					</motion.div>
 
 					<motion.div
